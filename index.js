2894e819262b0fbcb369f524acd8b157f000aab7
MritulAich <mridulaich332@gmail.com>
2024-06-29 18:52:54 +0600

    'delete' added

--- index.js
+++ index.js
@@ -25,15 +25,22 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+
+
         const roomCollection = client.db('hotelDB').collection('rooms');
 
         app.get('/rooms', async (req, res) => {
             const cursor = roomCollection.find();
             const result = await cursor.toArray();
             res.send(result);
-          })
+        })
 
-
+        app.delete('/room/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await roomCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
